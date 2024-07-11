2625f9c12fdc10372b17cb1bdd1af5f5cbe44189
marc-os89 <matquinhos1017@gmail.com>
2024-07-11 01:16:04 -0300

    commit do projeto indexa-agenda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,62 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ContainerComponent } from './componentes/container/container.component';
+import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
+import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
+
+interface Contato {
+  id: number
+  nome: string
+  telefone: string
+}
+
+import agenda from './agenda.json'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    FormsModule,
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+
 })
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Contato[] = agenda;
+
+  filtroPorTexto: string = '';
+
+  // Remove os acentos de uma string
+
+  private removerAcentos(texto: string): string {
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
+  filtrarContatosPorTexto (): Contato[] {
+    if (!this.filtroPorTexto) {
+      return this.contatos
+    } else {
+      return this.contatos.filter(contato => {
+        return this.removerAcentos(contato.nome).toLowerCase().includes(this.removerAcentos(this.filtroPorTexto).toLowerCase());
+      })
+    }
+  }
+
+  filtrarContatosPorLetraInicial(letra: string) : Contato[] {
+    return this.filtrarContatosPorTexto().filter( contato => {
+      // Compara a letra inicial sem considerar acentuações
+      return this.removerAcentos(contato.nome).toLowerCase().startsWith(this.removerAcentos(letra).toLowerCase());
+    })
+  }
 }
